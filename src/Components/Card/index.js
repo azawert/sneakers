9dbbd26461ea7e514ebe16330efa23ab9ce705ae
Igor <azawertik@gmail.com>
2022-06-04 00:25:36 +0300

    fixed skeleton

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from './Card.module.scss'
 import ContentLoader from "react-content-loader";
-const Card = ({addToCart,img,name,price,id,addToFavourite,favourited = false,added = false,loading=false}) => {
+const Card = ({addToCart,img,name,price,id,addToFavourite,favourited = false,added = false}) => {
     let [isAdded, setIsAdded] = React.useState(added);
     let [isAddedToFav,setFav] = React.useState(favourited);
     const addedToCart = () => {
@@ -15,21 +15,6 @@ const Card = ({addToCart,img,name,price,id,addToFavourite,favourited = false,add
     }
     return (
         <div className={styles.card}>
-            {
-                loading ? <ContentLoader
-                    speed={2}
-                    width={165}
-                    height={265}
-                    viewBox="0 0 155 265"
-                    backgroundColor="#f3f3f3"
-                    foregroundColor="#ecebeb"
-                >
-                    <rect x="1" y="0" rx="10" ry="10" width="155" height="155" />
-                    <rect x="0" y="167" rx="5" ry="5" width="155" height="15" />
-                    <rect x="0" y="187" rx="5" ry="5" width="100" height="15" />
-                    <rect x="1" y="234" rx="5" ry="5" width="80" height="25" />
-                    <rect x="124" y="230" rx="10" ry="10" width="32" height="32" />
-                </ContentLoader> : <>
                     <div className={styles.favourite}>
                         <img onClick={addedToFav} src={isAddedToFav ?'img/heartlike.svg' : 'img/heartdislike.svg'}/>
                     </div>
@@ -42,9 +27,6 @@ const Card = ({addToCart,img,name,price,id,addToFavourite,favourited = false,add
                         </div>
                         <button className='button' onClick={addedToCart  }><img src={isAdded ? "/img/buttonchecked.svg": '/img/buttonunchecked.svg'}/></button>
                     </div>
-                </>
-            }
-
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,27 @@
 import Card from "../Components/Card";
-import React from 'react'
+import React from 'react';
+import ContentLoader from "react-content-loader";
 function Home({sneakers,searchValue,onChangeSearchInput,setSearchValue,onAddToCart,onAddToFavourites,sneakersInCart,isLoading}){
+    const arr =[...Array(6)];
+    const newArr = arr.map(el=> {
+        return <ContentLoader
+            speed={2}
+            width={205}
+            height={295}
+            viewBox="0 0 155 265"
+            backgroundColor="#f3f3f3"
+            foregroundColor="#ecebeb"
+        >
+            <rect x="1" y="0" rx="10" ry="10" width="155" height="155" />
+            <rect x="0" y="167" rx="5" ry="5" width="155" height="15" />
+            <rect x="0" y="187" rx="5" ry="5" width="100" height="15" />
+            <rect x="1" y="234" rx="5" ry="5" width="80" height="25" />
+            <rect x="124" y="230" rx="10" ry="10" width="32" height="32" />
+        </ContentLoader>
+    })
     const renderSneakers = () => {
         const filteredItems = sneakers && sneakers.filter(sneaker=>sneaker.name.toLowerCase().includes(searchValue.toLowerCase()))
-       return isLoading ? [...Array(10)]: filteredItems.map((element)=>
+       return isLoading ? newArr : filteredItems.map((element)=>
            <Card
             key={element.img}
             added={sneakersInCart.some(item => item.img===element.img )}
